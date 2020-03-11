1a4e86389c8f9aeed40da4376b31a81c99708c25
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-03-11 23:43:02 +0900

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import IndexPage from './IndexPage';
+import HelloPage from './HelloPage';
+import NotFoundPage from './NotFoundPage';
 
-function App() {
-  return <div className="App">App</div>;
+export default class App extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={IndexPage}></Route>
+          <Route exact path="/hello" component={HelloPage}></Route>
+          <Route component={NotFoundPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
